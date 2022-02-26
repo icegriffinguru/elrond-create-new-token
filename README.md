6e20af5eb0e6b579010a2e7ff4b6e459797c91ac
ZenoKay <zenokay@protonmail.com>
2022-02-26 02:56:28 -0500

    Fix devnet and move to args token info

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
+# ESDT create
 ## About
 Create new ESDT using erdpy commands.
 
-## Problems
-- Only works on testnet, but not work on devnet
-On devnet, 400 - Bad Request Error occurred.
+## How to
+### Create PEM
+First you will need to create a wallet.pem file. To do so, you need to go on the network wallet of you choice (testnet, devnet, mainnet) and create a wallet. Keep the pass mnemonic.
+
+Then you can call the following command to generate the .pem file:
+
+```sh
+erdpy wallet derive --mnemonic wallet/devnet/devnet-wallet.pem 
+```
+
+### Running
+Now you can run the script with the following arguement.
+Make sure you don't have any space of special character in the TOKEN_NAME & TOKEN_TICKER. 
+```sh
+sh ./create-new-token.sh <NETWORK> <TOKEN_NAME> <TOKEN_TICKER>
+```
+
+Running with python:
+```py
+python3 create-new-token.py -net <NETWORK> -tn <TOKEN_NAME> -tt <TOKEN_TICKER>
+```
--- create-new-token.py
+++ create-new-token.py
@@ -3,15 +3,8 @@ import subprocess
 import ast
 import json
 import time
+import argparse
 
-
-OWNER_PEMFILE = "./wallet/testnet/testnet-wallet.pem"
-PROXY_URL = "https://testnet-api.elrond.com"
-GATEWAY_URL = "https://testnet-gateway.elrond.com"
-
-# OWNER_PEMFILE = "./wallet/devnet/devnet-wallet.pem"
-# PROXY_URL = "https://devnet-api.elrond.com"
-# GATEWAY_URL = "https://devnet-gateway.elrond.com"
 
 ESDT_ISSUE_ADDRESS = "erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u"
 
@@ -24,17 +17,18 @@ ESDT_ISSUE_ADDRESS = "erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a
 # Smart: @canFreeze@true@canWipe@true@canPause@true@canMint@true@canBurn@true@canUpgrade@true
 # Raw: @63616e467265657a65@74727565@63616e57697065@74727565@63616e5061757365@74727565@63616e4d696e74@74727565@63616e4275726e@74727565@63616e55706772616465@74727565
 
-def IssueTokenTransaction():
+def IssueTokenTransaction(name, ticker):
     data = "issue"
-    data += "@536e6f77466c616b65"   # token name: "SnowFlake"
-    data += "@534643"               # token ticker: "SFC"
-    data += "@d3c21bcecceda1000000" # initial supply: 1e24 wei - 1e6 $ESDT
-    data += "@12"                   # number of decimals: 18
+    data += f"@{name.encode('utf-8').hex()}"    # token name
+    data += f"@{ticker.upper().encode('utf-8').hex()}"  # token ticker
+    data += "@d3c21bcecceda1000000"             # initial supply: 1e24 wei - 1e6 $ESDT
+    data += "@12"                               # number of decimals: 18
     data += "@63616e467265657a65@74727565@63616e57697065@74727565@63616e5061757365@74727565@63616e4d696e74@74727565@63616e4275726e@74727565@63616e55706772616465@74727565"
 
     command = "erdpy tx new "
     command += " --pem=\"{0}\" ".format(OWNER_PEMFILE)
     command += " --proxy=\"{0}\" ".format(PROXY_URL)
+    command += " --chain=\"{0}\" ".format(CHAIN_ID)
     command += " --data=\"{0}\" ".format(data)
 
     command += " --receiver {0} ".format(ESDT_ISSUE_ADDRESS)
@@ -44,32 +38,32 @@ def IssueTokenTransaction():
     command += "--recall-nonce "
     command += " --send "
 
-    # print("Command: {0}".format(command))
 
     process = subprocess.run(command, shell = True, stdout = subprocess.PIPE)
     json_tx = process.stdout
-
-    # print("json_tx: {0}".format(json_tx))
-    
     return json_tx
 
 
 def GetTransactionStatus():
     query = GATEWAY_URL + "/transaction/" + hash_value + "/status?withResults=true"
     request = requests.get(query)
-    
+
     return request
 
+def _pick_chain_id(network):
+    if network == 'devnet': return 'D'
+    if network == 'testnet': return 'T'
+    if network == 'mainnet': return '1'
+    print('Error wrong ntwork name (devnet, testnet, mainnet).')
 
-def main():
-    tx_output = IssueTokenTransaction()
-    print()
-    
+def main(token_name, token_ticker):
+    tx_output = IssueTokenTransaction(token_name, token_ticker)
+
     tx_output = tx_output.decode("UTF-8")
     tx_dict = ast.literal_eval(tx_output)
     global hash_value
     hash_value = tx_dict["hash"]
-    
+
     # waiting for the end of the transaction
     wait_seconds = 20
     print("Waiting for {0} seconds...".format(wait_seconds))
@@ -77,7 +71,7 @@ def main():
 
     req = GetTransactionStatus()
     req_dict = json.loads(req.text)
-    
+
     print("IssueTokenTransaction status : ", req_dict.get("data", {}).get("status"))
     print("Status: {0}".format(req_dict))
 
@@ -85,4 +79,22 @@ def main():
     # i.e. "SFC-55ba74"
 
 if __name__ == "__main__":
-    main()
+    # Initialize parser
+    parser = argparse.ArgumentParser()
+    # Adding optional argument
+    parser.add_argument("-net", "--network", help="Network name (mainnet, devnet, testnet)")
+    # See here for the format:
+    # https://docs.elrond.com/developers/esdt-tokens/#parameters-format
+    parser.add_argument("-tn", "--tokenname", help="Token name (no special characters or spaces in it)")
+    parser.add_argument("-tt", "--tokenticker", help="Token ticker (no special characters or spaces in it)")
+    # Read arguments from command line
+    args = parser.parse_args()
+
+    OWNER_PEMFILE = f"./wallet/{args.network}/{args.network}-wallet.pem"
+    PROXY_URL = f"https://{args.network}-api.elrond.com"
+    subdomain_gateway = args.network+'-' if args.network != 'mainnet' else ''
+
+    GATEWAY_URL = f"https://{subdomain_gateway}gateway.elrond.com"
+    CHAIN_ID = _pick_chain_id(args.network)
+
+    main(args.tokenname, args.tokenticker)
